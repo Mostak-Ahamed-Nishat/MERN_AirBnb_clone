fdf000773d62e18b50e1d11ea1b54310ca3e6aa1
Mostak-Ahamed-Nishat <mostakahamed484@gmail.com>
2024-05-02 11:39:36 +0600

    Reservation already booked date block for other user added

--- app/actions/getReservations.ts
+++ app/actions/getReservations.ts
@@ -1,3 +1,57 @@
 import prisma from "@/app/libs/prismadb";
 
+interface IParams {
+  listingId?: string;
+  userId?: string;
+  authorId?: string;
+}
 
+export default async function getReservations(params: IParams) {
+  try {
+    const { listingId, userId, authorId } = params;
+
+    const query: any = {};
+
+    //if we have listingId find all the reservation for that listing
+    if (listingId) {
+      query.listingId = listingId;
+    }
+
+    //If  we have userId find all the reservation that active user made
+    if (userId) {
+      query.userId = userId;
+    }
+
+    //If  we have authorId. find all the reservation that someone booked in author listing
+    if (authorId) {
+      query.listing = { userId: authorId };
+    }
+
+    //Get the reservation from db
+    const reservations = await prisma.reservation.findMany({
+      where: query,
+      include: {
+        listing: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    //Make it safe data type
+    const safeReservations = reservations.map((reservation) => ({
+      ...reservation,
+      createdAt: reservation.createdAt.toISOString(),
+      startDate: reservation.startDate.toISOString(),
+      endDate: reservation.endDate.toISOString(),
+      listing: {
+        ...reservation.listing,
+        createdAt: reservation.listing.createdAt.toISOString(),
+      },
+    }));
+
+    return safeReservations;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,16 +1,21 @@
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import getListingById from "@/app/actions/getListingById";
+import getReservations from "@/app/actions/getReservations";
 import EmptyState from "@/components/EmptyState";
 import ListingClient from "@/components/listing/ListingClient";
 import React from "react";
+import { SafeReservation } from "../../types/index";
 
 interface IParams {
   listingId?: string;
+  reservations?: SafeReservation[];
 }
 
 const ListingPage = async ({ params }: { params: IParams }) => {
   const listing = await getListingById(params);
-  // const reservations = await getReservation(params);
+
+  const reservations = await getReservations(params);
+
   const currentUser = await getCurrentUser();
 
   if (!listing || listing == null) {
@@ -21,7 +26,7 @@ const ListingPage = async ({ params }: { params: IParams }) => {
     <ListingClient
       listing={listing}
       currentUser={currentUser}
-      // reservations={reservations}
+      reservations={reservations}
     />
   );
 };
