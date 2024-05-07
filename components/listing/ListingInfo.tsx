8399d95dbc70234d29243e7c7f3dad45bcc6681e
Mostak-Ahamed-Nishat <mostakahamed484@gmail.com>
2024-05-08 01:37:20 +0600

    packages update

--- components/listing/ListingInfo.tsx
+++ components/listing/ListingInfo.tsx
@@ -47,7 +47,8 @@ function ListingInfo({
       <div className="flex flex-col gap-2">
         <div className=" text-xl font-semibold flex flex-row items-center gap-2">
           <div>Hosted by {user?.name}</div>
-          <Avatar src={user?.image} userName={user?.name} />
+          <Avatar src={user?.image} />
+          {/* userName={user?.name} */}
         </div>
         <div className="flex flex-row items-center gap-4 font-light text-neutral-500">
           <p>{guestCount} guests</p>
