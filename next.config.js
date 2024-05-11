47aef6dd206cd12ced1d2fc810d6090710b57b70
Mostak-Ahamed-Nishat <mostakahamed484@gmail.com>
2024-05-11 16:38:39 +0600

    removed output from nextconfig

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
-
-=======
- output: process.env.NODE_ENV === "development" ? undefined : "export",
->>>>>>> 0a0cb2bb8e6cd2c75920c06657e9eddb6174c24f
   images: {
     domains: [
       "avatars.githubusercontent.com",
